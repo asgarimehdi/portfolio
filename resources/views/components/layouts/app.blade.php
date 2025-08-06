1971c7df700712e03d391ee164c7e8139387e4a1
asgarimehdi <mehdi.asgari@gmail.com>
2025-08-06 22:15:03 +0330

    avatar

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/cropper.min.css') }}"/>
 
+
+    <script src="{{ asset('js/cropper.min.js') }}" defer></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen font-sans antialiased bg-base-200">
